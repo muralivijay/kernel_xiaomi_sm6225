0c35a9f197ba03072cb0ad1449de6e921782c6a3
lucaswei <lucaswei@google.com>
2021-01-06 11:39:36 +0800

    Revert "Reverting below patches from android-4.19-stable.125"
    This reverts commit d6bbbe015730 ("Reverting below patches
    from android-4.19-stable.125")
    
    Bug: 172988823
    Bug: 176525217
    Change-Id: I2f907c951c5368caac5fc767d2f52fe9d7528d76
    Signed-off-by: lucaswei <lucaswei@google.com>
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- include/linux/cpu_cooling.h
+++ include/linux/cpu_cooling.h
@@ -44,10 +44,13 @@ struct cpu_cooling_ops {
 struct thermal_cooling_device *
 cpufreq_cooling_register(struct cpufreq_policy *policy);
 
-
 #ifdef CONFIG_ARCH_QCOM
 void cpu_limits_set_level(unsigned int cpu, unsigned int max_freq);
 #endif
+
+struct thermal_cooling_device *
+cpufreq_platform_cooling_register(struct cpufreq_policy *policy,
+					struct cpu_cooling_ops *ops);
 
 /**
  * cpufreq_platform_cooling_register - create cpufreq cooling device with
@@ -72,6 +75,13 @@ static inline struct thermal_cooling_device *
 cpufreq_cooling_register(struct cpufreq_policy *policy)
 {
 	return ERR_PTR(-ENOSYS);
+}
+
+static inline struct thermal_cooling_device *
+cpufreq_platform_cooling_register(struct cpufreq_policy *policy,
+					struct cpu_cooling_ops *ops)
+{
+	return NULL;
 }
 
 static inline
